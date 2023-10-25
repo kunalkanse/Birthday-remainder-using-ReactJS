21b2f4a61f98fd44470935d68da99779dd165a45
kunal kanse <kanseks008@gmail.com>
2023-10-26 01:51:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { Data } from "./Data";
 
 function App() {
+  const [data, setdata] = useState(Data);
+  // useEffect(()=>{
+  //           fetch(`${Data}`).then(res=>res.json()).then(d=>setdata(d));
+
+  // },[data]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="background">
+      <div className="mobile_outter">
+        <div className="inner_">
+          <h2>Today's Birthday {data.length}</h2>
+          {data &&
+            data.map((e) => {
+              return (
+                <section key={e.title}>
+                  <img src={e.img} alt="" />
+                  <div>
+                    <h3>{e.title}</h3>
+                    <h5>{e.age} years</h5>
+                  </div>
+                </section>
+              );
+            })}
+          <button
+            className="clear"
+            onClick={() => {
+              setdata([]);
+            }}
+          >
+            clear
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
---
+++ src/Data.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Data =[
+{title:'Kunal Kanse',img:'https://media.licdn.com/dms/image/D5603AQEOovMEh4nC-Q/profile-displayphoto-shrink_800_800/0/1690258253822?e=1703721600&v=beta&t=makF8Po3T-olO_u9EhxDnM7GcpDW7S0s3CtcTQh1LWE' , age:27},
+
+{title:'Aditya More',img:'https://media.licdn.com/dms/image/C4D03AQFd6EGHR6TIvA/profile-displayphoto-shrink_800_800/0/1661022088401?e=1703721600&v=beta&t=uklGin97o6vrjVfLHfWoJ1uGdM0UFcvAJcPqM-Saj_0',age:25},
+
+{title:'Swapnil Sapkal',img:'https://media.licdn.com/dms/image/C5603AQF1oDcYC8Hq7w/profile-displayphoto-shrink_800_800/0/1624421301250?e=1703721600&v=beta&t=2752ERuEyhzF8oB-GfCvp0aKlU-nAZFgbU51L_N6fvI', age:35},
+
+{title:'Amit Kanse',img:'https://media.licdn.com/dms/image/C5103AQGEWEGvQnydkQ/profile-displayphoto-shrink_800_800/0/1559026558513?e=1703721600&v=beta&t=XKsjgAUeNhYgtclvMkA2dAdqqELayKBFguvT9-bLhH4' , age:21},
+
+{title:'Mohan Sakat',img:'https://media.licdn.com/dms/image/C4E03AQGf1mlQY09eKA/profile-displayphoto-shrink_800_800/0/1517397289674?e=1703721600&v=beta&t=tEQ6N1qbPckqrC_I1Js1dIZ7HYwADwshTL6b5_VqQxs' , age:35},
+
+{title:'Omkar More',img:'https://media.licdn.com/dms/image/C5603AQEzA-xzBPLVeQ/profile-displayphoto-shrink_800_800/0/1604927345692?e=1703721600&v=beta&t=IZ7l_pTPHFUvQJCdIc9sp8Q1muqiK0zqTcPc7crpYLk' , age:22},
+
+
+];
